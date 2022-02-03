9edce61572af235a1fbcbcaf22a5bba0f214f9a6
Fihaa <haikalwatampone3@gmail.com>
2022-02-03 15:05:43 +0800

    Configuration Navigation

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View, SafeAreaView } from "react-native";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+}
