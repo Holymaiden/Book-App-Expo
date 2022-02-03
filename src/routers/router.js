642bf342ead4bce050f5ba3dfaf69393f36fc29c
Fihaa <haikalwatampone3@gmail.com>
2022-02-03 17:36:13 +0800

    Install

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,20 +2,51 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 import { Home } from "../screens";
+import { View } from "react-native";
+import Shadow from "../styles/Shadows";
 
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
-function router() {
+function HomeStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator
+      shifting={false}
+      initialRouteName="HOME"
+      headerMode="none"
+      barStyle={{
+        backgroundColor: "#ffffff",
+        marginHorizontal: wp(2),
+        position: "absolute",
+        bottom: hp(2.5),
+        elevation: 2,
+        borderRadius: 15,
+        height: hp(9),
+        position: "absolute",
+        overflow: "hidden",
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
   );
 }
 
-export default router;
+const Router = () => {
+  return (
+    <View style={{ flex: 1, width: "100%" }}>
+      <NavigationContainer>
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen name="Home" component={HomeStack} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+};
+
+export default Router;
