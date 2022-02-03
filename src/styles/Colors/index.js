7aa4cee575e92ce7ddf20306e7a97f5c9495095f
Fihaa <haikalwatampone3@gmail.com>
2022-02-03 15:26:51 +0800

    Added Colors Style

---
+++ src/styles/Colors/index.js
@@ -0,0 +1,12 @@
+const Colors = {
+  white: "#ffffff",
+  black: "#000000",
+  gray1: "#444A51",
+  gray2: "666666",
+  gray3: "#C4C4C4",
+  primary: "#00B0FF",
+  alert: "#FF4B2B",
+  warning: "#FF9F43",
+};
+
+export default Colors;
