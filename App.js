25e4182a6d9f10e2d5415bbe1c8f1fea9d4c0b0a
Fihaa <haikalwatampone3@gmail.com>
2022-02-03 17:36:55 +0800

    Added Button Tab

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Router from "./src/routers/router";
 
 export default function App() {
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,11 @@
-import { StatusBar } from "expo-status-bar";
+import { Appbar } from "react-native-paper";
 import { Text, View, SafeAreaView } from "react-native";
 
 export default function Home() {
   return (
     <SafeAreaView>
-      <View>
+      <View style={{ justifyContent: "center" }}>
         <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
       </View>
     </SafeAreaView>
   );
