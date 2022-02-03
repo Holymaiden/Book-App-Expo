416d4699acc7d9513843b043c45c8cb5c20dfc64
Fihaa <haikalwatampone3@gmail.com>
2022-02-03 15:28:43 +0800

    Install Material Buttom Tab

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
 import { Home } from "../screens";
 
